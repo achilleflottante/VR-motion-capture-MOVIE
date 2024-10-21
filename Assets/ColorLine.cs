f1b70dd8d9e6a773368b9fab627b5f6156953055
Valentin Allard <valentin.allard33@gmail.com>
2024-10-21 10:21:32 +0200

    Ajout d'une ligne de couleur

---
+++ Assets/ColorLine.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Line : MonoBehaviour
+{
+    LineRenderer lr;
+    public Transform Point1;
+    public Transform Point2;
+    public float Saturation;
+    public float Value;
+    public float DistanceMax;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.positionCount = 2;
+        lr.SetPosition(0, Point1.position);
+        lr.SetPosition(1, Point2.position);
+        lr.material.color = Color.HSVToRGB(hue(Vector3.Distance(Point1.position, Point2.position)) / 360f, Saturation / 100f, Value / 100f);
+    }
+    float hue(float distance)
+    {
+        float h;
+        if (distance > DistanceMax)
+        {
+            h = 0;
+        }
+        else
+        {
+            h = (1 - distance / DistanceMax) * 120;
+        }
+        return h;
+    }
+}
